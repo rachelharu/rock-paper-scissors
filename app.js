729801d50a0b3f3c0e5a6e78f4c2ea4107174847
rachelharu <rachel.haru9@gmail.com>
2021-10-28 21:50:59 -0700

    added playRound function as well as computerPlay() solving the bug where function would return the same output instead of it being random

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
+const playerSelection = "Rock";
+
+
 
 function computerPlay() {
-    const plays = ["rock", "paper", "scissors"];
-    return plays[Math.floor(Math.random()*plays.length)];
+  let plays = ["rock", "paper", "scissors"];
+  return plays[Math.floor(Math.random() * plays.length)]
 }
 
 
 
+
+const computerSelection = computerPlay()
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
-        console.log('paper beat rock')
-    }
-     else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
-         console.log('its a tie, try again!')
-    }
-     else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        console.log('rock beats scissors')
-    }
-    else {
-        console.log('wut')
-    }
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+    console.log('paper beat rock')
+    return ('paper beats rock')
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+    console.log('its a tie, try again!')
+    return ('its a tie, try again!')
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+    console.log('rock beats scissors')
+    return ('rock beats scissors')
+  } else {
+    console.log('wut')
+  }
+}
+
+
+
+function game() {
+  playRound(playerSelection, computerPlay());
+  playRound(playerSelection, computerPlay());
+  playRound(playerSelection, computerPlay());
+  playRound(playerSelection, computerPlay());
 };
 
-
-const playerSelection = "Rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
