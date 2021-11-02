30d089523780d0765d86f036681b145d058a4b34
rachelharu <rachel.haru9@gmail.com>
2021-11-02 14:32:22 -0700

    add comments above functions

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const playerSelection = "Rock";
 
-
-
+// function that returns random output from an array
 function computerPlay() {
   let plays = ["rock", "paper", "scissors"];
   return plays[Math.floor(Math.random() * plays.length)]
 }
 
-
-
-
 const computerSelection = computerPlay()
 
+//function that compares playerSelection and computerSelection to choose a winner
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
     console.log('paper beat rock')
@@ -28,7 +25,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-
+// function that plays a five round game of rock, paper, scissors
 function game() {
   playRound(playerSelection, computerPlay());
   playRound(playerSelection, computerPlay());
