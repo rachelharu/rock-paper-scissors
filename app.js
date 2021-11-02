8ce2e9b9c15438935d79475eb7ad2b256b63b640
rachelharu <rachel.haru9@gmail.com>
2021-11-02 16:00:15 -0700

    finish create updateScore function, edit game function, edit playRound function

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const playerSelection = "Rock";
+let playerScore = 0
+let computerScore = 0
+
+//defines button and uses the button click to start game/round
+let button = document.getElementById("start")
+button.onclick = game;
+
+
 
 // function that returns random output from an array
 function computerPlay() {
@@ -9,28 +16,50 @@ function computerPlay() {
 const computerSelection = computerPlay()
 
 //function that compares playerSelection and computerSelection to choose a winner
+
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+//     console.log('paper beat rock')
+//     return ('you lose!')
+//   } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+//     console.log('its a tie, try again!')
+//     return ('its a tie, try again!')
+//   } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+//     console.log('rock beats scissors')
+//     return ('you win!')
+//   } else {
+//     console.log('wut')
+//   }
+// }
+
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-    console.log('paper beat rock')
-    return ('paper beats rock')
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
-    console.log('its a tie, try again!')
-    return ('its a tie, try again!')
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-    console.log('rock beats scissors')
-    return ('rock beats scissors')
-  } else {
-    console.log('wut')
+  switch (true) {
+    case playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors':
+    case playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock':
+    case playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper':
+      return 'win'
+    case playerSelection.toLowerCase() === computerSelection:
+      return `It's a tie!`
+    default:
+      return 'lose'
   }
 }
 
 
 // function that plays a five round game of rock, paper, scissors
 function game() {
-  playRound(playerSelection, computerPlay());
-  playRound(playerSelection, computerPlay());
-  playRound(playerSelection, computerPlay());
-  playRound(playerSelection, computerPlay());
-};
+  const playerSelection = prompt();
+  const result = playRound(playerSelection, computerPlay())
+  console.log(result)
+  updateScore(result)
+  if (playerScore >= 5 || computerScore >= 5) {
+    alert('game over')
+  }
+}
 
-// console.log(playRound(playerSelection, computerSelection));
+
+
+function updateScore(result) {
+  if (result === 'win') playerScore++
+  else if (result === 'lose') computerScore++
+}
